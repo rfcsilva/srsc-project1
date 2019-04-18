842eadb8c554c8bf909b5ce155692ac3b882df41
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2019-04-18 18:42:40 +0100

    Added method to load cipher suit

--- src/secureSocket/SecureDatagramSocket.java
+++ src/secureSocket/SecureDatagramSocket.java
@@ -1,17 +1,25 @@
 package secureSocket;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.util.Properties;
 
 public class SecureDatagramSocket implements java.io.Closeable {
 
-	private DatagramSocket socket;
+	private static final String CIPHERSUITE_CONFIG_PATH = "configs/server/ciphersuite.conf";
 
+	private DatagramSocket socket;
+	private Properties ciphersuit_properties = new Properties();
+	
 	public SecureDatagramSocket(int port, InetAddress laddr) throws IOException {
+		
 		if( laddr.isMulticastAddress() ) {
 			MulticastSocket ms = new MulticastSocket(port);
 			ms.joinGroup(laddr);
@@ -19,27 +27,40 @@ public class SecureDatagramSocket implements java.io.Closeable {
 		} else {
 			socket = new DatagramSocket(port, laddr);
 		}
+		loadCipherSuitConfig();
 	}
-	
+
 	public SecureDatagramSocket(InetSocketAddress addr) throws IOException {
 		this(addr.getPort(), addr.getAddress());
 	}
 
 	public SecureDatagramSocket() throws IOException {
 		socket = new DatagramSocket();
+		loadCipherSuitConfig();
 	}
 
 	@Override
 	public void close() throws IOException {
 		socket.close();
 	}
-	
+
 	public void receive(DatagramPacket p) throws IOException {
 		socket.receive(p);
 	}
-	
+
 	public void send(DatagramPacket p) throws IOException {
 		socket.send(p);
 	}
 
+	private boolean loadCipherSuitConfig() {
+		
+		try {
+			InputStream inputStream = new FileInputStream(CIPHERSUITE_CONFIG_PATH);
+			ciphersuit_properties.load(inputStream);
+			return true;
+		} catch (IOException e) {	
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
