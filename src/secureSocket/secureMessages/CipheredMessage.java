ac5411473b4da2deb4da631f0bf189d2e098a0bd
andreffrosa <andreffrosa@hotmail.com>
2019-04-19 21:38:17 +0100

    Added CipheredMessage

---
+++ src/secureSocket/secureMessages/CipheredMessage.java
@@ -0,0 +1,21 @@
+// TODO : find better name for the class
+public class CipheredMessage implements Payload {
+	
+	// TODO
+	
+	public CipheredMessage() {
+		// TODO
+	}
+	
+	public static byte payloadType() {
+		return 0x01;
+	}
+	
+	public byte[] serialize() {
+		// TODO
+	}
+	
+	public int size() {
+		// TODO
+	}
+}
