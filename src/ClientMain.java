3bfc03dd7ec1bccd558f601c39cb567c52252c88
André Rosa <af.rosa@campus.fct.unl.pt>
2019-04-24 22:25:23 +0100

    fixed errors

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -1,16 +1,19 @@
-package kdc;
+
 
 import java.net.InetSocketAddress;
 
+import cryptography.CryptoFactory;
+import cryptography.Cryptography;
+import kdc.KDCServer;
 import kdc.needhamSchroeder.NeedhamSchroederClient;
 import kdc.needhamSchroeder.NeedhamSchroederServer;
 
-public class ClientMain {
+public class ClientMain { //
 
 	public static void main(String[] args) throws Exception {
 
 		InetSocketAddress kdc_addr = new InetSocketAddress("localhost", 8888);
-		InetSocketAddress b_addr = new InetSocketAddress("localhost", 6666);
+		InetSocketAddress b_addr = new InetSocketAddress( "localhost", 8889);
 		
 		if(args[0].equals("client") ) {
 			System.out.println("Client ready");
@@ -18,8 +21,11 @@ public class ClientMain {
 			nsc.getSessionParameters();	
 		}else {
 			System.out.println("Server ready");
-			NeedhamSchroederServer ncs = new NeedhamSchroederServer(b_addr);
-			ncs.getSessionParameters();
+			String config_file = "./configs/server/ciphersuite.conf";
+			Cryptography master_cryptoManager = CryptoFactory.loadFromConfig(config_file);
+			
+			KDCServer kdc_server = new NeedhamSchroederServer(b_addr, master_cryptoManager);
+			Cryptography session_cryptoManager = kdc_server.getSessionParameters();
 		}
 	}
 }
