e64fb244f1bcd082a49f4278cfdc8f366165ae7e
André Rosa <af.rosa@campus.fct.unl.pt>
2019-04-26 20:12:28 +0100

    exceptions

---
+++ src/keyEstablishmentProtocol/needhamSchroeder/exceptions/UnkonwnServerException.java
@@ -0,0 +1,19 @@
+package keyEstablishmentProtocol.needhamSchroeder.exceptions;
+
+public class UnkonwnServerException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public UnkonwnServerException() {
+		super();
+	}
+	
+	public UnkonwnServerException(String message) {
+		super(message);
+	}
+	
+}
---
+++ src/keyEstablishmentProtocol/needhamSchroeder/exceptions/WrongMessageTypeException.java
@@ -0,0 +1,19 @@
+package keyEstablishmentProtocol.needhamSchroeder.exceptions;
+
+public class WrongMessageTypeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public WrongMessageTypeException() {
+		super();
+	}
+	
+	public WrongMessageTypeException(String message) {
+		super(message);
+	}
+	
+}
