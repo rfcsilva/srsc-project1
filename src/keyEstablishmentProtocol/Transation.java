cae745b2866314bff54510c0f2045013107e865d
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2019-04-27 16:48:07 +0100

    Added tsn

--- src/keyEstablishmentProtocol/Transation.java
+++ src/keyEstablishmentProtocol/Transation.java
@@ -1,55 +1,141 @@
 package keyEstablishmentProtocol;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
 import java.util.Base64;
+
+import cryptography.CryptographyDoubleMac;
+import secureSocket.exceptions.InvalidMacException;
+import util.Utils;
 
 public class Transation {
 	private static final String SEPARATOR = ";";
 	// {A, B, Na, Args, InnerMac_Ka}
-	
+
+	private static final String INVALID_INNER_MAC = "Invalid Inner Mac";
+
 	private String a;
 	private String b;
 	private long na;
 	private String[] args;
 	private byte[] innerMac;
-	
+
 	public Transation(String a, String b, long na, String[] args, byte[] innerMac) {
-		
+
 		this.a = a;
 		this.b = b;
 		this.na = na;
 		this.args = args;
 		this.innerMac = innerMac;
-		
+
 	}
-	
+
 	public String toString() {
-		
+
 		String asString = a.concat(SEPARATOR).concat(b).concat(SEPARATOR);
 		asString =  asString.concat(String.valueOf(na)).concat(SEPARATOR);
 		for(String arg : args) {
 			asString = asString.concat(arg).concat(SEPARATOR);
 		}
-		
+
 		return asString.concat(Base64.getEncoder().encodeToString(innerMac));
-		
+
 	}
-	
+
 	public Transation fromString(String asString) {
-		
+
 		String[] parts = asString.split(SEPARATOR);
 		String a = parts[0];
 		String b = parts[1];
 		long nc = Long.parseLong(parts[2]);
-		
+
 		String[] args = new String[ parts.length - 4];
 		for(int i = 3; i < parts.length -1; i++) {
 			args[i-3] = parts[i];
 		}
-		
+
 		innerMac = Base64.getDecoder().decode(parts[parts.length-1]);
-	
+
 		return new Transation(a, b, nc, args, innerMac);
 	}
-	
-	
+
+	public byte[] serialize() throws IOException {
+
+		ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+		DataOutputStream dataOut = new DataOutputStream(byteOut);
+
+		dataOut.writeUTF(a);
+		dataOut.writeUTF(b);
+		dataOut.writeLong(na);
+
+		dataOut.writeInt(args.length);
+		for(String arg: args)
+			dataOut.writeUTF(arg);
+
+		dataOut.flush();
+		byteOut.flush();
+
+		byte[] data = byteOut.toByteArray();
+
+		dataOut.close();
+		byteOut.close();
+
+		return Utils.concat(data, innerMac);
+	}
+
+	public static Transation deserialize(CryptographyDoubleMac cryptoManager, byte[] rawData) throws IOException, InvalidMacException, InvalidKeyException {
+
+		byte[][] messageParts = cryptoManager.splitIntegrityProof(rawData);
+		if (!cryptoManager.validateIntegrityProof(messageParts[0], messageParts[1]))
+			throw new InvalidMacException(INVALID_INNER_MAC);
+		else {
+
+			ByteArrayInputStream byteIn = new ByteArrayInputStream(rawData);
+			DataInputStream dataIn = new DataInputStream(byteIn);
+
+			String a = dataIn.readUTF();
+			String b = dataIn.readUTF();
+			long na = dataIn.readLong();
+
+			int length = dataIn.readInt();
+			String[] arguments = new String[length];
+			for(int i = 0; i < length; i++) {
+				arguments[i] = dataIn.readUTF();
+			}
+
+			length = dataIn.readInt();
+			byte[] innerMac = new byte[length];
+			dataIn.read(innerMac, 0, length);
+
+			dataIn.close();
+			byteIn.close();
+
+			return new Transation(a, b, na, arguments, innerMac);
+
+		}
+	}
+
+	public String getA() {
+		return a;
+	}
+
+	public String getB() {
+		return b;
+	}
+
+	public long getNa() {
+		return na;
+	}
+
+	public String[] getArgs() {
+		return args;
+	}
+
+	public byte[] getInnerMac() {
+		return innerMac;
+	}
 }
--- src/keyEstablishmentProtocol/needhamSchroeder/NS1_Coins.java
+++ src/keyEstablishmentProtocol/needhamSchroeder/NS1_Coins.java
@@ -18,9 +18,9 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.ShortBufferException;
 
-import cryptography.AbstractCryptography;
 import cryptography.Cryptography;
 import cryptography.CryptographyDoubleMac;
+import keyEstablishmentProtocol.Transation;
 import keyEstablishmentProtocol.needhamSchroeder.exceptions.UnkonwnIdException;
 import keyEstablishmentProtocol.needhamSchroeder.exceptions.WrongCryptoManagerException;
 import secureSocket.exceptions.BrokenIntegrityException;
@@ -31,17 +31,17 @@ import util.Utils;
 
 public class NS1_Coins implements Payload {
 
+	private static final String MUST_USED_A_DOUBLE_MAC_CRYPTO_MANAGER = "Must used a Double MAC CryptoManager";
 	private static final String INVALID_OUTER_MAC = "Invalid Outer Mac";
-	private static final String INVALID_INNER_MAC = "Invalid Inner Mac";
+
 
 	public static final byte TYPE = 0x16;
 
+	//A || {A, B, Na, Args, InnerMac_Ka}ka || outerMac_Kma
+
+
 	// Payload data
-	private String a;
-	private String b;
-	private long na;
-	private String[] arguments;
-	private byte[] innerMac; // TODO: usar a mesma chave das cifras?
+	private Transation tns;
 	private byte[] cipherText;
 	private byte[] outerMac;
 	private byte[] message;
@@ -55,29 +55,23 @@ public class NS1_Coins implements Payload {
 			IllegalBlockSizeException, BadPaddingException, ShortBufferException, WrongCryptoManagerException {
 
 		if( !(cryptoManager instanceof CryptographyDoubleMac) ) 
-			throw new WrongCryptoManagerException("Must used a Double MAC CryptoManager");
+			throw new WrongCryptoManagerException(MUST_USED_A_DOUBLE_MAC_CRYPTO_MANAGER);
 
-		this.a = a;
-		this.b = b;
-		this.na = Na;
-		this.arguments = arguments;
+		byte[] msg = buildMessage(a, b, Na, serializeArgs(arguments));
+		byte[] innerMac = cryptoManager.computeIntegrityProof(msg);
 
-		byte[] msg = buildMessage(a, b, Na, serializeArgs(this.arguments));
-		this.innerMac = cryptoManager.computeIntegrityProof(msg);
+		tns = new Transation(a, b, Na, arguments, innerMac);
 
-		this.cipherText = cryptoManager.encrypt(Utils.concat(msg, innerMac));
+		this.cipherText = cryptoManager.encrypt(tns.serialize());
 
 		this.outerMac = cryptoManager.computeOuterMac(this.cipherText);
 
-		this.message = serializeFinnal(this.a, Utils.concat(cipherText, outerMac));
+		this.message = serializeFinnal(a, Utils.concat(cipherText, outerMac));
 	}
 
-	private NS1_Coins(String a, String b, long Na, String[] arguments, byte[] innerMac, byte[] cipherText, byte[] outerMac, byte[] message, CryptographyDoubleMac criptoManagerA, CryptographyDoubleMac criptoManagerB) {
-		this.a = a;
-		this.b = b;
-		this.na = Na;
-		this.arguments = arguments;
-		this.innerMac = innerMac;
+	private NS1_Coins(Transation tns, byte[] cipherText, byte[] outerMac, byte[] message, CryptographyDoubleMac criptoManagerA, CryptographyDoubleMac criptoManagerB) {
+
+		this.tns = tns;
 		this.cipherText = cipherText;
 		this.outerMac = outerMac;
 		this.message = message;
@@ -104,7 +98,7 @@ public class NS1_Coins implements Payload {
 		return data;
 	}
 
-	private byte[] serializeArgs(String[] args) throws IOException {
+	private byte[] serializeArgs(String[] arguments) throws IOException {
 		ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
 		DataOutputStream dataOut = new DataOutputStream(byteOut);
 
@@ -122,22 +116,6 @@ public class NS1_Coins implements Payload {
 		byteOut.close();
 
 		return data;
-	}
-
-	private static String[] deserializeArgs(byte[] raw_args) throws IOException {
-		ByteArrayInputStream byteIn = new ByteArrayInputStream(raw_args);
-		DataInputStream dataIn = new DataInputStream(byteIn);
-
-		int length = dataIn.readInt();
-		String[] arguments = new String[length];
-		for(int i = 0; i < length; i++) {
-			arguments[i] = dataIn.readUTF();
-		}
-
-		dataIn.close();
-		byteIn.close();
-
-		return arguments;
 	}
 
 	private static byte[] buildMessage(String a, String b, long Na, byte[] args) throws IOException {
@@ -198,33 +176,13 @@ public class NS1_Coins implements Payload {
 			throw new InvalidMacException(INVALID_OUTER_MAC);
 		else {
 			byte[] clearText = criptoManagerA.decrypt(messageParts[0]);
-			byte[][] messageParts2 = criptoManagerA.splitIntegrityProof(clearText);
-			if (!criptoManagerA.validateIntegrityProof(messageParts2[0], messageParts2[1]))
-				throw new InvalidMacException(INVALID_INNER_MAC);
-			else {
-				byteIn = new ByteArrayInputStream(messageParts2[0]);
-				dataIn = new DataInputStream(byteIn);
+			Transation tsn = Transation.deserialize(criptoManagerA, clearText);
 
-				a = dataIn.readUTF();
-				String b = dataIn.readUTF();
+			CryptographyDoubleMac criptoManagerB = ((CryptographyNS) criptoManager).getCryptographyFromId(tsn.getB());
 
-				long Na = dataIn.readLong();
-
-				int length = dataIn.readInt();
-				byte[] cipheredArgs = new byte[length];
-				dataIn.read(cipheredArgs, 0, length);
-
-				dataIn.close();
-				byteIn.close();
-
-				//AbstractCryptography criptoManagerA = ((CryptographyNS) criptoManager).getCryptographyFromId(a);
-				CryptographyDoubleMac criptoManagerB = ((CryptographyNS) criptoManager).getCryptographyFromId(b);
-
-				String[] arguments = deserializeArgs(cipheredArgs);
-
-				return new NS1_Coins(a, b, Na, arguments, messageParts2[1], messageParts[0], messageParts[1], cipherWithMac, criptoManagerA, criptoManagerB);
-			}
+			return new NS1_Coins(tsn, messageParts[0], messageParts[1], cipherWithMac, criptoManagerA, criptoManagerB);
 		}
+
 	}	
 
 	public CryptographyDoubleMac getCryptoManagerA() {
@@ -236,19 +194,19 @@ public class NS1_Coins implements Payload {
 	}
 
 	public String getA() {
-		return a;
+		return tns.getA();
 	}
 
 	public String getB() {
-		return b;
+		return tns.getB();
 	}
 
 	public long getNa() {
-		return na;
+		return tns.getNa();
 	}
 
 	public String[] getArgs() {
-		return arguments;
+		return tns.getArgs();
 	}
 
 }
